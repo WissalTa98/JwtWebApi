bede122a76e579d00e5bd43087ca7cecffe8fda6
MW638ST <twissal16@gmail.com>
2023-05-02 17:05:35 +0100

    Update User Model

--- Dto/User.cs
+++ Dto/User.cs
@@ -2,9 +2,14 @@
 {
 	public class User
 	{
+		public int Id { get; set; }
+		public string Email { get; set; } = string.Empty;
 		public string Username { get; set; } = string.Empty;
-		public byte[] PasswordHash { get; set; }
-		public byte[] PasswordSalt { get; set; }
+		public byte[] PasswordHash { get; set; } = new byte[32];
+		public byte[] PasswordSalt { get; set; } = new byte[32];
+		public DateTime? VerifiedAt { get; set; }
+		public string? PasswordResetToken { get; set; }
+		public DateTime? ResetTokenExpires { get; set; }
 
 		public string RefreshToken { get; set; } = string.Empty;
 		public DateTime TokenCreated { get; set; }
